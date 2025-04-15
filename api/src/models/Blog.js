298bbf82d7a7d9da0bff275717ceefdcfdf07c3a
mhatons <mhatons@gmail.com>
2025-04-15 17:40:09 +0100

    updated api again

--- api/src/models/Blog.js
+++ api/src/models/Blog.js
@@ -15,7 +15,7 @@ const BlogSchema = new mongoose.Schema({
   },
   author: { 
     type: mongoose.Schema.Types.ObjectId, 
-    ref: "User",
+    ref: "users",
     required: true
   },
   coverImage: { 
