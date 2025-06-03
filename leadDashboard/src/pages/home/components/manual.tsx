a8d895f26e4ef1fbe7b8c4a5e3b4229901d23333
mhatons <mhatons@gmail.com>
2025-06-03 12:26:04 +0100

    updated lead dashboard

--- leadDashboard/src/pages/home/components/manual.tsx
+++ leadDashboard/src/pages/home/components/manual.tsx
@@ -2,7 +2,7 @@ import { Cards } from "../../../assets";
 
 export default function Manual() {
     return (
-        <div className="bg-[#0f2d6c] sm:h-[calc(175vh-250px)] h-[calc(105vh-250px)] sm:px-16 px-2 py-16">
+        <div className="bg-[#0f2d6c]  sm:px-16 px-2 py-16">
             <div className="flex flex-col items-center ">
                 <h1 className="text-2xl md:text-3xl lg:text-5xl font-bold text-center text-white max-w-6xl mb-6">
                     Manual Lead Scoring is Holding You Back
