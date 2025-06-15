279ecb69b44dbad691fd8e21ccca69f850c5490d
mhatons <mhatons@gmail.com>
2025-06-15 20:27:16 +0100

    added scoring

---
+++ scoring/src/assets/index.ts
@@ -0,0 +1,14 @@
+// Icon section
+import sirzLogo from './img/sirz logo png 1.svg';
+import googleLog from './img/devicon_google.svg';
+import or from './img/Frame 1659.svg';
+import projectIcon from './img/Frame 1171275943.svg';
+
+import defaultProfileImg from './img/profile.svg';
+
+
+export const SirzLogo = sirzLogo;
+export const GoogleLog = googleLog;
+export const orLogo = or;
+export const ProjectIcon = projectIcon;
+export const DefaultProfileImg = defaultProfileImg;
---
+++ scoring/src/pages/home/automation/index.tsx
@@ -0,0 +1,11 @@
+import photo from '../../../assets/img/Frame 1766.svg'
+
+
+
+export default function Automation() {
+    return (
+    <div className="sm:w-[80%] w-[90%] mx-auto">
+      <img src={photo} alt="" />
+    </div>
+    );
+}
